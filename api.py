959c69650371274e5d7960572721829fe975cf49
pauluser95 <pauluser123@gmail.com>
2025-05-10 22:47:21 +0700

    add dataset feature

--- api.py
+++ api.py
@@ -1,13 +1,17 @@
-from flask import Flask, jsonify, request, render_template, url_for
+from flask import Flask, jsonify, request, render_template, url_for, send_file
 import useModel
 import os
 import urllib.parse
 import base64
+import datetime
+import zipfile
+from io import BytesIO
 
 app = Flask(__name__)
 
 saveString = []
 saveResults = []
+IMAGE_FOLDER = 'static'
 
 @app.route("/")
 def hello_world():
@@ -30,6 +34,37 @@ def add_income():
     saveResults.append(result)
     return jsonify(saveString[0]),200
 
+@app.route('/saveDataset', methods=['POST'])
+def saveDataset():
+    saveString.clear()
+    saveResults.clear()
+    image=request.get_json()["name"]
+    image = urllib.parse.unquote(image)
+    image = image + "=" * ((4 - len(image) % 4) % 4)
+    imageDate = datetime.datetime.now()
+    imageName = "static/"+str(imageDate.strftime("%Y%m%d%M%S"))+".png"
+    with open(imageName, "wb") as fh:
+        fh.write(base64.urlsafe_b64decode(image))
+    print(imageName+" saved")
+    return jsonify(imageName+" saved"),200
+
+@app.route('/showDataset', methods=['GET'])
+def showDataset():
+    files = os.listdir(IMAGE_FOLDER)
+    image_files = [f for f in files if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+    return render_template('all-dataset.html', images=image_files)
+
+@app.route('/download_all')
+def download_all():
+    # Create in-memory zip file
+    buffer = BytesIO()
+    with zipfile.ZipFile(buffer, 'w') as zipf:
+        for filename in os.listdir(IMAGE_FOLDER):
+            if filename.lower().endswith(('.jpg', '.png', '.jpeg', '.gif')):
+                filepath = os.path.join(IMAGE_FOLDER, filename)
+                zipf.write(filepath, arcname=filename)
+    buffer.seek(0)
+    return send_file(buffer, as_attachment=True, download_name='images.zip', mimetype='application/zip')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
